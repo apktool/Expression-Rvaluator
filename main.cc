62464d74261c4111ee3b9e112627181355eab69d
CentOS7 <apktoolOo@gmail.com>
2016-08-18 07:58:06 +0800

    do ... while

--- main.cc
+++ main.cc
@@ -1,5 +1,19 @@
 #include<iostream>
+#include<string>
+#include"Node.h"
+#include"Scanner.h"
+#include"Parser.h"
 
 int main(int argc, char* argv[]){
+	do{
+		std::cout<<"> ";
+		std::string buf;
+		std::getline(std::cin,buf);
+		std::cout<<buf<<std::endl;
+		Scanner scanner(buf);
+		Parser parser(scanner);
+		parser.Parse();
+		parser.Calculate();
+	}while(1);
 	return 0;
 }
