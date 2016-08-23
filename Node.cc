0bc93969e3e0a586deeefdfe97b9b6a0ae33dea1
CentOS7 <apktoolOo@gmail.com>
2016-08-23 10:54:34 +0800

    SumNode,ProductNode|取消AddNode,SubNode,MultiplyNode,DivideNode

--- Node.cc
+++ Node.cc
@@ -1,6 +1,7 @@
 #include"Node.h"
 #include<iostream>
 #include<cmath>
+#include<cassert>
 
 double NumberNode::Calc() const{
 	return number_;
@@ -14,7 +15,7 @@ BinaryNode::~BinaryNode(){
 UnaryNode::~UnaryNode(){
 	delete child_;
 }
-
+/*
 double AddNode::Calc() const{
 	return left_->Calc()+right_->Calc();
 }
@@ -36,7 +37,51 @@ double DivideNode::Calc() const{
 		return HUGE_VAL; //头文件<cmath>中定义的一个最大数
 	}
 }
-
+*/
 double UMinusNode::Calc() const{
 	return -child_->Calc();
+}
+
+MultipleNode::~MultipleNode(){
+	std::vector<Node*>::const_iterator it;
+	for(it=childs_.begin();it!=childs_.end();++it){
+		delete *it;
+	}
+}
+
+double SumNode::Calc() const{
+	double result=0.0;
+	std::vector<Node*>::const_iterator childIt=childs_.begin();
+	std::vector<bool>::const_iterator positiveIt=positives_.begin();
+	for(;childIt!=childs_.end();++childIt,++positiveIt){
+		assert(positiveIt!=positives_.end());
+		double val=(*childIt)->Calc();
+		if(*positiveIt){
+			result+=val;
+		}else{
+			result-=val;
+		}
+	}
+	assert(positiveIt==positives_.end());
+	return result;
+}
+
+double ProductNode::Calc() const{
+	double result=1.0;
+	std::vector<Node*>::const_iterator childIt=childs_.begin();
+	std::vector<bool>::const_iterator positiveIt=positives_.begin();
+	for(;childIt!=childs_.end();++childIt,++positiveIt){
+		assert(positiveIt!=positives_.end());
+		double val=(*childIt)->Calc();
+		if(*positiveIt){
+			result*=val;
+		}else if(val!=0.0){
+			result/=val;
+		}else{
+			std::cout<<"Division by zero."<<std::endl;
+			return HUGE_VAL;
+		}
+	}
+	assert(positiveIt==positives_.end());
+	return result;
 }
