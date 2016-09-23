e1470e8c3114edf0478914ad2d976bf21775e70d
CentOS7 <apktoolOo@gmail.com>
2016-09-23 16:35:54 +0800

    class|Calc

---
+++ Calc.h
@@ -0,0 +1,21 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+#include"SymbolTable.h"
+#include"Storage.h"
+
+class Parser;
+class Calc{
+	friend class Parser;
+	public:
+		Calc():storage_(symTbl_){}
+	private:
+		Storage& GetStorage(){
+			return storage_;
+		}
+		unsigned int AddSymbol(const std::string& str);
+		unsigned int FindSymbol(const std::string& str) const;
+		SymbolTable symTbl_;
+		Storage storage_;
+};
+
+#endif //_CALC_H_
