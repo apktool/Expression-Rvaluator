af91113b55d956e5f62214a959aca3c27dc3d556
CentOS7 <apktoolOo@gmail.com>
2016-09-23 16:35:09 +0800

    修正运行错误buf|if语句

--- main.cc
+++ main.cc
@@ -3,18 +3,28 @@
 #include"Node.h"
 #include"Scanner.h"
 #include"Parser.h"
+#include"Calc.h"
 
 int main(int argc, char* argv[]){
-	STATUS status=STATUS_OK;
+	Calc calc;
+	STATUS status;
 	do{
 		std::cout<<"> ";
 		std::string buf;
 		std::getline(std::cin,buf);
 		//std::cout<<buf<<std::endl;
 		Scanner scanner(buf);
-		Parser parser(scanner);
-		parser.Parse();
-		std::cout<<parser.Calculate()<<std::endl;
+		if(!scanner.IsEmpty()){
+			Parser parser(scanner,calc);
+			status=parser.Parse();
+			if(status==STATUS_OK){
+				std::cout<<parser.Calculate()<<std::endl;
+			}else{
+				std::cout<<"Syntax Error."<<std::endl;
+			}
+		}else{
+			std::cout<<"Expression is empty"<<std::endl;
+		}
 	}while(status!=STATUS_QUIT);
 	return 0;
 }
