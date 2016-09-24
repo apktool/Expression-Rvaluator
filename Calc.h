7f5f7b557b6d93a3e1dc12c96b6f5580cd222856
CentOS7 <apktoolOo@gmail.com>
2016-09-24 20:36:55 +0800

    GetFunction(),IsFunction()

--- Calc.h
+++ Calc.h
@@ -2,19 +2,27 @@
 #define _CALC_H_
 #include"SymbolTable.h"
 #include"Storage.h"
+#include"FunctionTable.h"
 
 class Parser;
 class Calc{
 	friend class Parser;
 	public:
-		Calc():storage_(symTbl_){}
+		Calc():funTbl_(symTbl_),storage_(symTbl_){}
 	private:
 		Storage& GetStorage(){
 			return storage_;
 		}
+		PtrFun GetFunction(unsigned int id) const{
+			return funTbl_.GetFunction(id);
+		}
+		bool IsFunction(unsigned int id) const{
+			return id<funTbl_.Size();
+		}
 		unsigned int AddSymbol(const std::string& str);
 		unsigned int FindSymbol(const std::string& str) const;
 		SymbolTable symTbl_;
+		FunctionTable funTbl_;
 		Storage storage_;
 };
 
