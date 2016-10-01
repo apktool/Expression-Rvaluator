ac75de55b0f19d47aee94a1773b0a360be73865e
CentOS7 <apktoolOo@gmail.com>
2016-10-01 22:54:05 +0800

    添加对特殊的命令的处理代码

--- main.cc
+++ main.cc
@@ -5,39 +5,47 @@
 #include"Parser.h"
 #include"Calc.h"
 #include"Exception.h"
+#include"CommandParser.h"
 
 int main(int argc, char* argv[]){
 	Calc calc;
 	STATUS status=STATUS_OK;
 	do{
 		std::cout<<"> ";
-		std::string buf;
-		std::getline(std::cin,buf);
+		//std::string buf;
+		//std::getline(std::cin,buf);
 		//std::cout<<buf<<std::endl;
-		Scanner scanner(buf);
+		//Scanner scanner(buf);
+		Scanner scanner(std::cin);
 		if(!scanner.IsEmpty()){
-			Parser parser(scanner,calc);
-
-			try{
-				status=parser.Parse();
-				if(status==STATUS_OK){	//此处可以不需要判定，属于C语言的思想
-					std::cout<<parser.Calculate()<<std::endl;
-				}
-			}catch(SyntaxError& se){
-				std::cout<<se.what()<<std::endl;
-				//std::cout<<se.StackTrace()<<std::endl;	//栈回溯
-			}catch(Exception& e){
-				std::cout<<e.what()<<std::endl;
-			}catch(...){
-				std::cout<<"Internal error."<<std::endl;
-			}
-
-			/*
-			if(status==STATUS_OK){
-				std::cout<<parser.Calculate()<<std::endl;
+			if(scanner.IsCommand()){
+				CommandParser parser(scanner,calc);
+				status=parser.Execute();
 			}else{
+
+				Parser parser(scanner,calc);
+
+				try{
+					status=parser.Parse();
+					if(status==STATUS_OK){	//此处可以不需要判定，属于C语言的思想
+						std::cout<<parser.Calculate()<<std::endl;
+					}
+				}catch(SyntaxError& se){
+					std::cout<<se.what()<<std::endl;
+					//std::cout<<se.StackTrace()<<std::endl;	//栈回溯
+				}catch(Exception& e){
+					std::cout<<e.what()<<std::endl;
+				}catch(...){
+					std::cout<<"Internal error."<<std::endl;
+				}
+
+				/*
+				if(status==STATUS_OK){
+					std::cout<<parser.Calculate()<<std::endl;
+				}else{
 				std::cout<<"Syntax Error."<<std::endl;
-			}*/
+				}*/
+			}
 		}else{
 			std::cout<<"Expression is empty"<<std::endl;
 		}
